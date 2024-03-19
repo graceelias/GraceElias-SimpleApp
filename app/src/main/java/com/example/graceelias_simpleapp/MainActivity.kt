9f9769af8b35a3d71ef443de68f08e11fc47a712
graceelias <91794990+graceelias@users.noreply.github.com>
2024-03-19 02:07:22 -0400

    simpleapp completed with sharedpreferences

--- app/src/main/java/com/example/graceelias_simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/graceelias_simpleapp/MainActivity.kt
@@ -4,27 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.graceelias_simpleapp.databinding.ActivityMainBinding
 import android.content.SharedPreferences
+import android.content.Context
 
 class MainActivity : AppCompatActivity()
 {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
-    private var buttonClicked = false
+    private var imageNum = 0
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        imageNum = sharedPreferences.getInt("imageNum", imageNum)
+        updateImage(imageNum)
+        binding.editText.setText(sharedPreferences.getString("text", ""))
+
         binding.button.setOnClickListener{
-            buttonClicked = true
-            updateImage()
+            imageNum = (Math.random()*5).toInt()
+            updateImage(imageNum)
         }
     }
 
-    private fun updateImage()
+    override fun onDestroy()
     {
-        val num = (Math.random()*5).toInt()
+        super.onDestroy()
+        val editor = sharedPreferences.edit()
+        editor.putInt("imageNum", imageNum)
+        editor.putString("text", binding.editText.text.toString())
+    }
+
+    private fun updateImage(num: Number)
+    {
         if(num == 0)
         {
             binding.imageView.setImageResource(R.drawable.boston_university_logo_0)
