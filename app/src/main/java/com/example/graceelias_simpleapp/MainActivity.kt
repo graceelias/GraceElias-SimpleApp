01b2af70cc917547d5900e22bf5e0f35f1f8668c
graceelias <91794990+graceelias@users.noreply.github.com>
2024-03-19 01:35:01 -0400

    app working without sharedpreferences

--- app/src/main/java/com/example/graceelias_simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/graceelias_simpleapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.graceelias_simpleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.graceelias_simpleapp.databinding.ActivityMainBinding
+import android.content.SharedPreferences
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+
+    private var buttonClicked = false
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            buttonClicked = true
+            updateImage()
+        }
+    }
+
+    private fun updateImage()
+    {
+        val num = (Math.random()*5).toInt()
+        if(num == 0)
+        {
+            binding.imageView.setImageResource(R.drawable.boston_university_logo_0)
+        }
+        else if(num == 1)
+        {
+            binding.imageView.setImageResource(R.drawable.boston_university_logo_1)
+        }
+        else if(num == 2)
+        {
+            binding.imageView.setImageResource(R.drawable.boston_university_logo_2)
+        }
+        else if(num == 3)
+        {
+            binding.imageView.setImageResource(R.drawable.boston_university_logo_3)
+        }
+        else if(num == 5)
+        {
+            binding.imageView.setImageResource(R.drawable.boston_university_logo_4)
+        }
     }
 }
